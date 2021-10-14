bae0fb20840a5392c1ab09a3ff04c77a224d189c
shard520 <65344196+shard520@users.noreply.github.com>
2021-10-14 16:53:36 +0100

    Add remaining functionality and convert prompts to use inquirer so the program can be run from the command line.

--- cyberpet.js
+++ cyberpet.js
@@ -1,3 +1,5 @@
+const inquirer = require('inquirer');
+
 let myPet;
 
 class Animal {
@@ -6,6 +8,7 @@ class Animal {
   _thirst;
   _boredom;
   _isAlive = true;
+  _status;
 
   constructor(name) {
     this._name = name;
@@ -13,6 +16,7 @@ class Animal {
     this._hunger = 0;
     this._thirst = 0;
     this._boredom = 0;
+    this._status = 'Your pet is alive and well.';
   }
 
   get name() {
@@ -30,7 +34,6 @@ class Animal {
   get boredom() {
     return this._boredom;
   }
-
   get stats() {
     return console.table({
       name: this._name,
@@ -40,49 +43,97 @@ class Animal {
       boredom: this._boredom,
     });
   }
+  get status() {
+    return this._status;
+  }
+
+  set status(msg) {
+    this._status = msg;
+  }
 
   checkStats() {
-    if (
-      this.health <= 0 ||
-      this.hunger >= 50 ||
-      this.thirst >= 50 ||
-      this.boredom >= 50
-    ) {
+    if (this._health <= 0) {
       this._isAlive = false;
+      this.status =
+        'Your pet has died from malnutrition, please make sure your next pet has plenty to eat and drink.';
+    }
+    if (this._boredom >= 50) {
+      this._isAlive = false;
+      this.status = `${this.name} has run away to join the circus because they were bored, please play with your pet more next time...`;
     }
   }
 
   drinks() {
-    this._thirst -= 5;
-    if (this._thirst < 0) this._thirst = 0;
-    return this._thirst;
+    this.thirst(-5);
+    if (this.thirst < 0) this._thirst = 0;
+    return this.thirst;
   }
 
-  eats() {
-    const food = prompt(`
-What do you want to feed ${this.name}?
-a)Snack: Hunger -2
-b)Meal: Hunger -5
-c)Treat: Hunger -10
-`);
+  async eats() {
+    const { food } = await inquirer.prompt({
+      type: 'list',
+      name: 'food',
+      message: `What do you want to feed ${this.name}?`,
+      choices: [
+        {
+          key: 'a',
+          name: 'Snack hunger -2',
+          value: 'snack',
+        },
+        {
+          key: 'b',
+          name: 'Meal hunger -5',
+          value: 'meal',
+        },
+        {
+          key: 'c',
+          name: 'Treat hunger -10',
+          value: 'treat',
+        },
+      ],
+    });
+
     if (food === 'snack') this.hunger(-2);
     if (food === 'meal') this.hunger(-5);
     if (food === 'treat') this.hunger(-10);
-
-    if (this._hunger < 0) this._hunger = 0;
 
     return this._hunger;
   }
 
   hunger(num) {
     this._hunger += num;
+
+    if (this._hunger < 0) this._hunger = 0;
+    if (this._hunger > 25) this.health(5);
+    if (this._hunger > 40) {
+      this.health(5);
+      this.boredom -= 5;
+    }
+    return this.checkStats();
+  }
+
+  thirst(num) {
+    this._thirst += num;
+
+    if (this._thirst < 0) this._thirst = 0;
+    if (this._thirst > 25) this.health(5);
+    if (this._thirst > 40) {
+      this.health(5);
+      this.boredom -= 5;
+    }
+    return this.checkStats();
+  }
+
+  health(num) {
+    this._health += num;
+    if (this._health < 0) this._health = 0;
   }
 
   boredom(num) {
     this._boredom += num;
     if (this._boredom < 0) this._boredom = 0;
 
-    return this._boredom;
+    return this.checkStats();
   }
 }
 class Cat extends Animal {
@@ -90,95 +141,211 @@ class Cat extends Animal {
     super(name);
   }
 
-  play() {
-    const choice = prompt(`
-Do you want to play with:
-a) tin foil ball: boredom -2
-b) wind up mouse: boredom -5
-c) ball of woll: boredom -10
-`).toLowerCase();
+  async play() {
+    const { play } = await inquirer.prompt({
+      type: 'list',
+      name: 'play',
+      message: 'Do you want to play with?',
+      choices: [
+        {
+          key: 'a',
+          name: 'Tin foil ball: boredom -2',
+          value: 'foil',
+        },
+        {
+          key: 'b',
+          name: 'wind up mouse: boredom -5',
+          value: 'mouse',
+        },
+        {
+          key: 'c',
+          name: 'Ball of wool: boredom -10',
+          value: 'wool',
+        },
+      ],
+    });
 
-    if (choice === 'a') this.boredom(-2);
-    if (choice === 'b') this.boredom(-5);
-    if (choice === 'c') this.boredom(-10);
+    if (play === 'foil') this.boredom(-2);
+    if (play === 'mouse') this.boredom(-5);
+    if (play === 'wool') this.boredom(-10);
   }
 }
 class Dog extends Animal {
   constructor(name) {
     super(name);
   }
-  play() {
-    const choice = prompt(`
-Do you want to play with:
-a) Stick: boredom -2
-b) Chewy toy: boredom -5
-c) Ball : boredom -10
-`).toLowerCase();
 
-    if (choice === 'a') this.boredom(-2);
-    if (choice === 'b') this.boredom(-5);
-    if (choice === 'c') this.boredom(-10);
+  async play() {
+    const { play } = await inquirer.prompt({
+      type: 'list',
+      name: 'play',
+      message: 'Do you want to play with?',
+      choices: [
+        {
+          key: 'a',
+          name: 'Stick: boredom -2',
+          value: 'stick',
+        },
+        {
+          key: 'b',
+          name: 'Ball: boredom -5',
+          value: 'ball',
+        },
+        {
+          key: 'c',
+          name: 'Chase the postman: boredom -10',
+          value: 'chase',
+        },
+      ],
+    });
+
+    if (play === 'stick') this.boredom(-2);
+    if (play === 'ball') this.boredom(-5);
+    if (play === 'chase') this.boredom(-10);
   }
 }
 class Rabbit extends Animal {
   constructor(name) {
     super(name);
   }
-  play() {
-    const choice = prompt(`
-Do you want to play with:
-a) Straw: boredom -2
-b) Paper Towels: boredom -5
-`).toLowerCase();
+  async play() {
+    const { play } = await inquirer.prompt({
+      type: 'list',
+      name: 'play',
+      message: 'Do you want to play with?',
+      choices: [
+        {
+          key: 'a',
+          name: 'Straw: boredom -2',
+          value: 'straw',
+        },
+        {
+          key: 'b',
+          name: 'Paper towels: boredom -5',
+          value: 'paper',
+        },
+      ],
+    });
 
-    if (choice === 'a') this.boredom(-2);
-    if (choice === 'b') this.boredom(-5);
+    if (play === 'straw') this.boredom(-2);
+    if (play === 'paper') this.boredom(-5);
   }
 }
 class Parrot extends Animal {
   constructor(name) {
     super(name);
   }
-  play() {
-    const choice = prompt(`
-Do you want to play with:
-a) Learn a word: boredom -2
-b) Learn a swear word: boredom -5
-c) Swear at the mail man: boredom -10
-`).toLowerCase();
+  async play() {
+    const { play } = await inquirer.prompt({
+      type: 'list',
+      name: 'play',
+      message: 'Do you want to play with?',
+      choices: [
+        {
+          key: 'a',
+          name: 'Learn a word: boredom -2',
+          value: 'word',
+        },
+        {
+          key: 'b',
+          name: 'Learn a swear word: boredom -5',
+          value: 'swear',
+        },
+        {
+          key: 'c',
+          name: 'Swear at the mail man: boredom -10',
+          value: 'mail',
+        },
+      ],
+    });
 
-    if (choice === 'a') this.boredom(-2);
-    if (choice === 'b') this.boredom(-5);
-    if (choice === 'c') this.boredom(-10);
+    if (play === 'word') this.boredom(-2);
+    if (play === 'swear') this.boredom(-5);
+    if (play === 'mail') this.boredom(-10);
   }
 }
-function start() {
-  let typeOfPet = prompt(
-    'What type of pet would you like? Please choose from: Cat, Dog, Rabbit, Parrot.'
-  ).toLowerCase();
-  console.log(typeOfPet);
-  let petName = prompt('Please enter a name for you pet.');
-  console.log(petName);
+async function start() {
+  const { typeOfPet } = await inquirer.prompt({
+    type: 'list',
+    name: 'typeOfPet',
+    message:
+      'What type of pet would you like? Please choose from the following options:',
+    choices: [
+      {
+        key: 'a',
+        name: 'Cat',
+        value: 'cat',
+      },
+      {
+        key: 'b',
+        name: 'Dog',
+        value: 'dog',
+      },
+      {
+        key: 'c',
+        name: 'Rabbit',
+        value: 'rabbit',
+      },
+      {
+        key: 'd',
+        name: 'Parrot',
+        value: 'parrot',
+      },
+    ],
+  });
+
+  const { petName } = await inquirer.prompt({
+    type: 'input',
+    name: 'petName',
+    message: 'What is your pet called?',
+  });
+
   if (typeOfPet === 'cat') myPet = new Cat(petName);
   else if (typeOfPet === 'dog') myPet = new Dog(petName);
   else if (typeOfPet === 'rabbit') myPet = new Rabbit(petName);
   else if (typeOfPet === 'parrot') myPet = new Parrot(petName);
 
-  myPet.boredom(45);
-  myPet.hunger(45);
   userChoice();
 }
 
-function userChoice() {
+async function userChoice() {
   myPet.boredom(2);
   myPet.hunger(2);
+  myPet.thirst(2);
+  myPet.stats;
 
-  const choice = prompt(`
-What would you like to do? Please choose from: Play, Eat, Drink
-To see your pet's stats type stats`).toLowerCase();
+  const { choice } = await inquirer.prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'What would you like to do?',
+    choices: [
+      {
+        key: 'a',
+        name: 'Play with your pet',
+        value: 'play',
+      },
+      {
+        key: 'b',
+        name: 'Feed your pet',
+        value: 'feed',
+      },
+      {
+        key: 'c',
+        name: 'Give your pet a drink',
+        value: 'drink',
+      },
+      {
+        key: 'd',
+        name: "View your pet's status",
+        value: 'status',
+      },
+    ],
+  });
 
-  if (choice === 'stats') myPet.stats;
-  if (choice === 'play') myPet.play();
+  if (choice === 'status') console.log(myPet.status);
+  if (choice === 'play') await myPet.play();
+  if (choice === 'feed') await myPet.eats();
+  if (choice === 'drink') await myPet.drinks();
 
   myPet.checkStats();
 
@@ -199,9 +366,7 @@ start();
 
 function gameOver(scenario) {
   if (scenario === 'boredom') {
-    console.log(
-      `${myPet.name} has run away to join the circus because they were bored, please play with your pet more next time...`
-    );
+    console.log();
   } else if (scenario === 'hunger') {
     console.log(
       `${myPet.name} has run away to join the circus because they were bored, please play with your pet more next time...`
